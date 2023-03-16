cb25bda08b99a74c59e25700c9fd952be4ac059b
QAVoosh01 <harthick@voosh.in>
2023-03-16 15:32:29 +0530

    saved

---
+++ src/main/java/newfolder/newthing/App.java
@@ -0,0 +1,13 @@
+package newfolder.newthing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
